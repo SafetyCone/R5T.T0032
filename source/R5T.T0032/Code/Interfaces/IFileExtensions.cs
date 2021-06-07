5f3976de7059d35db12936fdc8b4004f7957736f
David Coats <david@getsafetycone.com>
2021-06-07 15:54:21 -0700

    Add IFileExtensions base for extension methods with implementation.

---
+++ source/R5T.T0032/Code/Interfaces/IFileExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.T0032
+{
+    /// <summary>
+    /// Empty interface to be used as a base for extension methods.
+    /// </summary>
+    public interface IFileExtensions
+    {
+    }
+}
