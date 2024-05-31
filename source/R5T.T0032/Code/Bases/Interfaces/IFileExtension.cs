44f48ef38d24ebf08f986ef3769de5d179b565f9
davidcoats <david@getsafetycone.com>
2024-05-30 22:25:13 -0700

    Interim changes.

--- source/R5T.T0032/Code/Bases/Interfaces/IFileExtension.cs
+++ source/R5T.T0032/Code/Bases/Interfaces/IFileExtension.cs
@@ -6,6 +6,7 @@ namespace R5T.T0032
     /// <summary>
     /// Empty interface to be used as a base for extension methods.
     /// </summary>
+    [Obsolete("See R5T.Z0072.Z000")]
     public interface IFileExtension
     {
     }
